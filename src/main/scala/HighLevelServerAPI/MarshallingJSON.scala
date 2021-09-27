502e2f95394ae9801100095efe34502bf5493469
Nazar <nazar.bukhari@agoda.com>
2021-09-27 16:43:39 +0700

    streaming

--- src/main/scala/HighLevelServerAPI/MarshallingJSON.scala
+++ src/main/scala/HighLevelServerAPI/MarshallingJSON.scala
@@ -1,23 +1,34 @@
 package HighLevelServerAPI
 
-import akka.actor.{Actor, ActorLogging, Props}
-import akka.actor.typed.ActorSystem
-import akka.actor.typed.scaladsl.Behaviors
-import akka.stream.ActorMaterializer
+import HighLevelServerAPI.GameAreaMap.{AddPlayer, GetPlayerByClass}
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import akka.http.scaladsl.server.Directives._
+import akka.pattern.ask
+import akka.util.Timeout
+import spray.json.DefaultJsonProtocol._
+import scala.concurrent.duration._
+import scala.language.postfixOps
 
 case class Player(nickName: String, characterClass: String, level: Int)
 
 object GameAreaMap { //Companion Object
   case object GetAllPlayers
+
   case class GetPlayer(nickName: String)
+
   case class GetPlayerByClass(characterClass: String)
+
   case class AddPlayer(player: Player)
+
   case class RemovePlayer(player: Player)
+
   case object OperationSuccess
 }
 
-class GameAreaMap extends Actor with ActorLogging{
+class GameAreaMap extends Actor with ActorLogging {
+
   import GameAreaMap._
+
   var players = Map[String, Player]()
 
   override def receive: Receive = { // Just like overriding runnable method in java for multithreading
@@ -42,9 +53,50 @@ class GameAreaMap extends Actor with ActorLogging{
 
 object MarshallingJSON extends App {
 
-  implicit val system = ActorSystem(Behaviors.empty, "my-system") //Needed for route
-  implicit val executionContext = system.executionContext
+  implicit val system = ActorSystem("my-system")
+
   import system.dispatchers
 
-  val gameMap = system.systemActorOf(Props[GameAreaMap],"GameAreaMap")
+  val gameMap = system.actorOf(Props[GameAreaMap], "GameAreaMap")
+  val playerList = List(
+    Player("Nazar", "C1", 10),
+    Player("Bukhari", "C2", 7),
+    Player("Alex", "C3", 5)
+  )
+
+  playerList.foreach(player =>
+    gameMap ! AddPlayer(player)
+  )
+
+//  GET /api/player
+//  GET /api/player/(nickname)
+//  GET /api/player?nickname=x
+//  GET /api/player/class/(characterclass)
+//  POST /api/player
+//  DELETE /api/player
+
+  implicit val timeout = Timeout(2 seconds)
+  implicit val PlayerFormat = jsonFormat3(Player)
+
+  val route =
+    pathPrefix("api" / "player") {
+      get {
+        (path(Segment) | parameter(Symbol("nickname")) ){ nickname =>
+          reject
+        } ~
+        path("class" / Segment){ characterclass =>
+          reject
+        } ~
+        pathEndOrSingleSlash{
+           //GET All Players
+            reject
+         }
+      } ~
+        post {
+          reject
+        } ~
+        delete {
+          reject
+        }
+    }
 }
---
+++ src/main/scala/HttpServerStreamingRandomNumbers.scala
@@ -0,0 +1,40 @@
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import akka.http.scaladsl.model.Uri.Path
+import akka.stream.scaladsl.Source
+import akka.http.scaladsl.server.Directives._
+import akka.util.ByteString
+
+import scala.io.StdIn
+import scala.util.Random
+
+object HttpServerStreamingRandomNumbers {
+  def main(args: Array[String]): Unit = {
+      implicit val system = ActorSystem(Behaviors.empty, "RandomNumberStream")
+      implicit val executionContext = system.executionContext
+
+    val numbers = Source.fromIterator(() =>
+        Iterator.continually(Random.nextInt()))
+
+    val route = path("random") {
+      get {
+        complete(
+          HttpEntity(
+            ContentTypes.`text/plain(UTF-8)`,
+            numbers.map(n => ByteString(s"$n\n"))
+          )
+        )
+      }
+    }
+//    streaming requests means that the server decides how fast the remote client can push the data of the request body.
+//    curl --limit-rate 50b 127.0.0.1:8080/random
+    
+    val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+    StdIn.readLine()
+    bindingFuture.
+      flatMap(_.unbind())
+      .onComplete(_ => system.terminate())
+  }
+}
